d21fcbdebe589678857b6d479c804149e7467ea0
Iain Hill <iainardernhill@gmail.com>
2017-03-10 11:37:44 +0100

    conditionals

---
+++ c2.02.6-52-ex2-10-lower.c
@@ -0,0 +1,40 @@
+/*
+ * Exercise 2-10. Rewrite the function lower, which converts upper case letters
+ * to lower case, with a conditional expression instead of if-else.
+ */
+#include <stdio.h>
+
+/* lower: convert c to lower case; ASCII only */
+int lower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return c + 'a' - 'A';
+	else
+		return c;
+}
+
+int lowerConditional(int c)
+{
+	return(c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
+
+int main(void)
+{
+	int c;
+	int x;
+
+	while((c = getchar()) != 'Q')
+	{
+		if(c != '\n') {
+			printf("if else : ");
+			x = lower(c);
+			putchar(x);
+			puts("");
+			printf("conditional : ");
+			x = lowerConditional(c);
+			putchar(x);
+			puts("");
+		}
+	}
+}
+
