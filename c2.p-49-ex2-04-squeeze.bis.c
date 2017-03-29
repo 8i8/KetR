dd3c5de17db8e5fc52db1b9e3aa5270c012de5bc
Iain Hill <iainardernhill@gmail.com>
2017-03-29 19:24:04 +0200

    Squeeze amended

--- c2.p-49-ex2-04-squeeze.bis.c
+++ c2.p-49-ex2-04-squeeze.bis.c
@@ -10,28 +10,33 @@
  * That aside, now redo based upon the squeese function that I apparently
  * missed of first reading.
  */
+
+/* Redefine getline */
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdio.h>
+#include <stdint.h>
 
-int myGetline(char string[], int lim);
-void sortString(char string2[], int len);
-void squeeze(char string[], char string2[], int len, int len2);
-int myGetline(char str[], int lim);
+static void sortString(char string2[], uint16_t len);
+static void squeeze(char string[], char string2[], uint16_t len, uint16_t len2);
+static uint16_t getline(char str[], uint16_t lim);
 
-#define MAXLENGTH	1000
+#define MAXLENGTH	65535
 
 int main(void)
 {
-	int len;
-	int len2;
+	uint16_t len;
+	uint16_t len2;
 	char string[MAXLENGTH];
 	char string2[MAXLENGTH];
 
 	printf("Please provide two strings, one after the other ...\n"
 			"String 1 :~ ");
-	len = myGetline(string, MAXLENGTH);
+	len = getline(string, MAXLENGTH);
 
 	printf("String two, whos characters will be removed from string one.\n");
-	len2 = myGetline(string2, MAXLENGTH);
+	len2 = getline(string2, MAXLENGTH);
 
 	sortString(string2, len2);
 	squeeze(string, string2, len, len2);
@@ -43,10 +48,10 @@ int main(void)
  * Search for each character in string2 in  string and replace it with '\0',
  * then use a bubble search to send all of the '\0's to the end of the string.
  */
-void squeeze(char string[], char string2[], int len, int len2)
+static void squeeze(char string[], char string2[], uint16_t len, uint16_t len2)
 {
-	int i;
-	int j;
+	size_t i;
+	size_t j;
 
 	for (i = 0; i < len2 && string2[i] != '~'; i++)
 	{
@@ -57,11 +62,10 @@ void squeeze(char string[], char string2[], int len, int len2)
 		}
 	}
 
-	int swapped;
-	int temp;
+	uint8_t swapped;
+	uint8_t temp;
 
-	do
-	{
+	do {
 		swapped = 0;
 		for (i = 1; i < len; i++)
 		{
@@ -80,11 +84,12 @@ void squeeze(char string[], char string2[], int len, int len2)
 /*
  * Put the string into alphabetical order and remove any double characters.
  */
-void sortString(char string[], int len)
+static void sortString(char string[], uint16_t len)
 {
-	int i;
-	int swapped;
-	int temp;
+	size_t i;
+	uint8_t swapped;
+	uint8_t temp;
+	uint8_t count = 0;
 
 	do
 	{
@@ -98,25 +103,33 @@ void sortString(char string[], int len)
 				string[i] = temp;
 				swapped = 1;
 			}
-			if(string[i-1] == string[i])
+			/*
+			 * Convert any duplicates to ~ so that they are moved
+			 * to the end of the array
+			 */
+			if(string[i-1] == string[i] && string[i] != '~') {
 				string[i] = '~';
+				count++;
+			}
 		}
 	}
 	while (swapped);
+	/* Remove the doubles that were converted to ~ */
+	string[len-1-count] = '\0';
 }
 
 /*
  * Get input line by line.
  */
-int myGetline(char str[], int lim)
+static uint16_t getline(char str[], uint16_t lim)
 {
-	int i;
+	size_t i;
 	int c;
 
-	for (i = 0; i < lim-1 && (c = getchar()) != 'Q' && c != '\n'; i++)
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++)
 		str[i] = c;
 
-	str[i] = '\0';
+	str[i++] = '\0';
 	return i;
 }
 
--- c2.p-49-ex2-04-squeeze.c
+++ c2.p-49-ex2-04-squeeze.c
@@ -1,31 +1,40 @@
 /*
  * Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes
  * each character in s1 that matches any character in the string s2.
+ *
+ * Two strings can be sent into the program using stdin from the command line
+ * by running printf into | pipe, such as:
+ *
+ * printf "Line one \n line two" | ./a.out
  */
+
+/* Redefine getline */
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdio.h>
+#include <stdint.h>
 
-int myGetline(char string[], int lim);
-void sortString(char string2[], int len);
-void squeeze(char string[], char string2[]);
-int myGetline(char str[], int lim);
+static void sortString(char string2[], uint8_t len);
+static void squeeze(char string[], char string2[]);
+static uint8_t getline(char str[], const uint8_t lim);
 
-#define MAXLENGTH	1000
+#define MAXLENGTH	255
 
 int main(void)
 {
-	int len;
-	int len2;
 	char string[MAXLENGTH];
 	char string2[MAXLENGTH];
+	uint8_t len;
 
 	printf("Please provide two strings, one after the other ...\n"
-			"String 1 :~ ");
-	len = myGetline(string, MAXLENGTH);
+			"String 1 :~ \n");
+	getline(string, MAXLENGTH);
 
 	printf("String two, whos characters will be removed from string one.\n");
-	len2 = myGetline(string2, MAXLENGTH);
+	len = getline(string2, MAXLENGTH);
 
-	sortString(string2, len2);
+	sortString(string2, len);
 	squeeze(string, string2);
 
 	printf("--> %s\n", string);
@@ -34,9 +43,9 @@ int main(void)
 /*
  * Remove all of the characters found in string two, from string one.
  */
-void squeeze(char string[], char string2[])
+static void squeeze(char string[], char string2[])
 {
-	int i, j, k;
+	size_t i, j, k;
 
 	for (i = 0; string2[i] != '\0'; i++)
 	{
@@ -50,16 +59,16 @@ void squeeze(char string[], char string2[])
 /*
  * Put the string into alphabetical order and remove any double characters.
  */
-void sortString(char string[], int len)
+static void sortString(char string[], uint8_t len)
 {
-	int i;
-	int swapped;
-	int temp;
+	size_t i;
+	uint8_t swapped;
+	uint8_t temp;
+	uint8_t count = 0;
 
-	do
-	{
+	do {
 		swapped = 0;
-		for (i = 1; i < len; i++)
+		for (i = 1; string[i] != '\0'; i++)
 		{
 			if(string[i-1] > string[i])
 			{
@@ -68,25 +77,36 @@ void sortString(char string[], int len)
 				string[i] = temp;
 				swapped = 1;
 			}
-			if(string[i-1] == string[i])
+			/*
+			 * Convert any duplicates to ~ so that they are moved
+			 * to the end of the array
+			 */
+			if(string[i-1] == string[i] && string[i] != '~') {
 				string[i] = '~';
+				count++;
+			}
 		}
 	}
 	while (swapped);
+	/* Remove the doubles that were converted to ~ */
+	string[len-1-count] = '\0';
 }
 
 /*
  * Get input line by line.
  */
-int myGetline(char str[], int lim)
+static uint8_t getline(char str[], const uint8_t lim)
 {
-	int i;
-	int c;
+	size_t i;
+	int8_t c;
 
-	for (i = 0; i < lim-1 && (c = getchar()) != 'Q' && c != '\n'; i++)
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++)
 		str[i] = c;
 
-	str[i] = '\0';
+	str[i++] = '\0';
+
+	fflush(stdin);
+
 	return i;
 }
 
