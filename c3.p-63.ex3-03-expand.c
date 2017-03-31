c54abc7b83ebf3ff57c182410e83df7400ebe5a0
Iain Hill <iainardernhill@gmail.com>
2017-03-31 10:45:17 +0200

    expand pragram made into expand(s1,s2) function.

--- c3.p-63.ex3-03-expand.c
+++ c3.p-63.ex3-03-expand.c
@@ -6,25 +6,52 @@
  * leading or trailing - is taken literally.
  */
 
+/* Redefine getline */
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
 
+#define MAXLEN		255
+
+static size_t __expandRead(const char read[], char s2[], size_t j);
+static size_t __checkState(const char read[], char s2[], const size_t j);
+static void __writeInputBuffer(char read[], const int8_t c);
+static void expand(const char s1[], char s2[]);
+static uint8_t getline(char str[], const size_t lim);
+
+int main(void)
+{
+	char s1[MAXLEN];
+	char s2[MAXLEN];
+
+	while ((getline(s1, MAXLEN)) > 0)
+	{
+		expand(s1, s2);
+		printf("%s", s2);
+	}
+
+	return 0;
+}
+
 /*
- * Expand the expression, either asscending of decending, return the number of
+ * Expand the expression, either ascending of descending, return the number of
  * char that have been added.
  */
-static uint8_t expandRead(const char read[])
+static size_t __expandRead(const char read[], char s2[], size_t j)
 {
-	int8_t i;
+	size_t i;
 	i = 0;
+	j -= 2;		/* Offset the first char and the hyphen */
 
 	if (read[0] < read[2])
-		while (read[0]+i < read[2])
-			printf("%c", read[0] + i++);
+		while (read[0] + i < read[2])
+			s2[j++] = read[0] + i++;
 	else if (read[0] > read[2])
 		while (read[0] - i > read[2])
-			printf("%c", read[0] - i++);
+			s2[j++] = read[0] - i++;
 	return i;
 }
 
@@ -32,16 +59,16 @@ static uint8_t expandRead(const char read[])
  * Check the char both before and after the hyphen, are they the same type? If
  * yes then expand the expression.
  */
-static uint8_t checkState(const char read[])
+static size_t __checkState(const char read[], char s2[], const size_t j)
 {
 	if (!isalnum(read[0]))
 		;
 	else if (islower(read[0]) && islower(read[2]))
-		return expandRead(read);
+		return __expandRead(read, s2, j);
 	else if (isupper(read[0]) && isupper(read[2]))
-		return expandRead(read);
+		return __expandRead(read, s2, j);
 	else if (isdigit(read[0]) && isdigit(read[2]))
-		return expandRead(read);
+		return __expandRead(read, s2, j);
 	return 0;
 }
 
@@ -49,44 +76,71 @@ static uint8_t checkState(const char read[])
  * Keep store of the last three characters entered, used to define the state
  * for expansion, if it is required.
  */
-static void writeInputBuffer(char read[], const int8_t c)
+static void __writeInputBuffer(char read[], const int8_t c)
 {
 	read[0] = read[1];
 	read[1] = read[2];
 	read[2] = c;
 }
 
-int main(void)
+/*
+ * Receive two strings assuming that they are of adequate length for the
+ * operation, iterate over string 1 transferring to string t, use a buffer of
+ * 3 char so that any 3 char expressions such as a-z can be expanded into the
+ * second string.
+ */
+static void expand(const char s1[], char s2[])
 {
-	int8_t  c;
-	uint8_t count;
+	size_t i, j;
 	char read[3];
+	uint8_t count, num;
+	i = j = 0;
 
 	/*
 	 * Create the offset for reading ahead 2 char, enables the expansion of
-	 * 3 char expressions such as a-c
+	 * 3 char expressions such as a-z.
 	 */
 	count = 2;
-	while ((c = getchar()) != EOF)
+	while(s1[i] != '\0')
 	{
-		writeInputBuffer(read, c);
+		__writeInputBuffer(read, s1[i]);
 
 		/* If expansion has just been made, skip over the hyphen */
-		if(count)
+		if(count) {
 			count--;
-		else {
+			s2[j++] = s1[i++];
+		} else {
 			if(read[1] == '-') {
-				if(checkState(read))
+				if((num = __checkState(read, s2, j)) > 0) {
 					count = 1;
-				else
-					putchar(read[0]);
+					j += num;
+					i++;
+				} else
+					s2[j++] = s1[i++];
 			} else {
-				putchar(read[0]);
+				s2[j++] = s1[i++];
 			}
 		}
 	}
-	putchar('\n');
+	s2[j] = '\0';
+}
 
-	return 0;
+/*
+ * Get input line by line.
+ */
+static uint8_t getline(char str[], const size_t lim)
+{
+	size_t i;
+	int8_t c;
+
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++)
+		str[i] = c;
+
+	if(c == '\n')
+		str[i++] = '\n';
+
+	str[i] = '\0';
+
+	return i;
 }
 
