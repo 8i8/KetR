dea02b0f52849baf5a9784a6cfed9e87276c7e9c
Iain Hill <iainardernhill@gmail.com>
2017-04-04 16:43:14 +0200

    function capacity expanded to prepare for variables

--- c4.p-76.ex4-05-RPN_calc-sine_exp_pow.c
+++ c4.p-76.ex4-05-RPN_calc-sine_exp_pow.c
@@ -13,14 +13,19 @@
 
 #define MAXOP	100
 #define NUMBER	'0'	/* A signal that a number was found. */
-#define SIN	1
-#define COS	2
-#define TAN	3
-#define EXP	4
-#define LOG	5
-#define POW	6
+#define SIN	1001
+#define COS	1002
+#define TAN	1003
+#define EXP	1004
+#define LOG	1005
+#define POW	1006
+#define COPY	1007
+#define DEL	1008
+#define CLEAR	1009
+#define PRINT	1010
+#define SWAP	1011
 
-static char getop(char []);
+static int16_t getop(char []);
 static void push(double);
 static double pop(void);
 static void printStack(void);
@@ -30,7 +35,7 @@ static void emptyStack(void);
 
 int main(void)
 {
-	char type;
+	int16_t type;
 	char s[MAXOP];
 	double op2;
 
@@ -65,22 +70,20 @@ int main(void)
 				else
 					printf("error: zero modulo\n");
 				break;
-			case 'c':
+			case COPY:
 				duplicate();
 				break;
-			case 'd':
+			case DEL:
 				pop();
 				break;
-			case 'e':
+			case CLEAR:
 				emptyStack();
 				break;
-			case 'p':
+			case PRINT:
 				printStack();
 				break;
-			case 's':
+			case SWAP:
 				swapStack();
-				break;
-			case 'x':
 				break;
 			case SIN:
 				push(sin( pop() ));
@@ -103,6 +106,8 @@ int main(void)
 				push(pow( pop(), op2 ));
 				break;
 			case '\n':
+				break;
+			case 0:
 				break;
 			default:
 				printf("error: unknown command %s\n", s);
@@ -204,13 +209,13 @@ static void emptyStack(void)
 static char getch(void);
 static void ungetch(char);
 static int8_t isToken(void);
-static int readToken(void);
+static int16_t readToken(void);
 static char tokenBuffer(char c);
 
 /*
  * getop: get next operator or numeric operand.
  */
-static char getop(char s[])
+static int16_t getop(char s[])
 {
 	size_t i;
 	char c;
@@ -334,26 +339,38 @@ static void clearText(void)
 /*
  * Read the text buffer.
  */
-static int readToken(void)
+static int16_t readToken(void)
 {
 	if (!strcmp(bufText, "sin")) {
 		clearText();
-		return 1;
+		return 1001;
 	} else if (!strcmp(bufText, "cos")) {
 		clearText();
-		return 2;
+		return 1002;
 	} else if (!strcmp(bufText, "tan")) {
 		clearText();
-		return 3;
+		return 1003;
 	} else if (!strcmp(bufText, "exp")) {
 		clearText();
-		return 4;
+		return 1004;
 	} else if (!strcmp(bufText, "log")) {
 		clearText();
-		return 5;
+		return 1005;
 	} else if (!strcmp(bufText, "pow")) {
 		clearText();
-		return 6;
+		return 1006;
+	} else if (!strcmp(bufText, "copy")) {
+		clearText();
+		return 1007;
+	} else if (!strcmp(bufText, "del")) {
+		clearText();
+		return 1008;
+	} else if (!strcmp(bufText, "print")) {
+		clearText();
+		return 1010;
+	} else if (!strcmp(bufText, "swap")) {
+		clearText();
+		return 1011;
 	}
 	clearText();
 	return 0;
@@ -372,7 +389,7 @@ static char tokenBuffer(char c)
 		} else {
 			pushChar(tolower(c));
 			token = 1;
-			return 'x';
+			return 0;
 		}
 	} else {
 		pushChar(tolower(c));
@@ -380,6 +397,6 @@ static char tokenBuffer(char c)
 			tokenBuffer(d);
 	}
 
-	return 'x';
+	return 0;
 }
 
