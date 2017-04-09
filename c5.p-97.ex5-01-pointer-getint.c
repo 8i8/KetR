c5e5e10a15058e876b69634882f867e47a0579bb
Iain Hill <iainardernhill@gmail.com>
2017-04-09 18:49:09 +0200

    pointers: getfloat and corrections to getint

---
+++ c5.p-97.ex5-01-pointer-getint.c
@@ -0,0 +1,70 @@
+/*
+ * Exercise 5-1. As written, getint treats a + or - not followed by a digit as
+ * a valid representation of zero. Fix it to push such a character back on the
+ * input.
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE	1000
+
+char buf[BUFSIZE];
+int buf_p;
+
+/*
+ * Get a (possibly pushed back) character.
+ */
+static int getch(void)
+{
+	return (buf_p > 0) ? buf[--buf_p] : getchar();
+}
+
+/*
+ * Push character back on input.
+ */
+static void ungetch(char c)
+{
+	if (buf_p >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[buf_p++] = c;
+}
+
+/*
+ * Getint uses a pointer to return an int and the return type of the function
+ * to return EOF.
+ */
+static int getint(int *pn)
+{
+	int c, sign;
+
+	while (isspace(c = getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-') {
+		c = getch();
+		for (*pn = 0L; isspace(c); c = getch())
+			;
+	}
+	for (*pn = 0L; isdigit(c); c = getch())
+		*pn = 10L * *pn + (double)(c - (int)'0');
+	*pn *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
+
+int main(void)
+{
+	int a;
+
+	getint(&a);
+	printf("%d\n", a);
+
+	return 0;
+}
+
---
+++ c5.p-97.ex5-02-pointer-getfloat.c
@@ -0,0 +1,93 @@
+/*
+ * Exercise 5-2. Write getfloat, the floating-point analog of getint. What type
+ * does getfloat return as its function value?
+ *
+ * Getr float also returns an int, this is used to pass back the EOF character.
+ */
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+#define BUFSIZE	255
+
+char buf[BUFSIZE];
+int buf_p;
+
+/*
+ * Get a (possibly pushed back) character.
+ */
+static int getch(void)
+{
+	return (buf_p > 0) ? buf[--buf_p] : getchar();
+}
+
+/*
+ * Push character back on input.
+ */
+static void ungetch(char c)
+{
+	if (buf_p >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[buf_p++] = c;
+}
+
+/*
+ * Getfloat uses a pointer to return an int and the return type of the function
+ * to return EOF.
+ */
+static int getfloat(double *pn)
+{
+	int c, sign;
+	double power;
+
+	while (isspace(c = getch()))
+		;
+	while (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-') {
+		c = getch();
+		for (*pn = 0; isspace(c); c = getch())
+				;
+	}
+
+	/* Collect digits before a '.' */
+	for (*pn = 0L; isdigit(c); c = getch())
+		*pn = 10L * *pn + (double)(c - (int)'0');
+
+	/* Continue to collect digits after a '.' */
+	if (c == '.') {
+		/* Skip over the character '.' return EOF if required */
+		c = getch();
+		if (c == EOF)
+			return c;
+		/* For each extra digit acquired, multiply 'power' by ten */
+		for (power = 1L; isdigit(c); c = getch()) {
+			*pn = 10L * *pn + (double)(c - (int)'0');
+			power *= 10L;
+		}
+	}
+
+	/*
+	 * Set the sign by multiplyinb by sign, and the decimal point by
+	 * deviding by power.
+	 */
+	*pn = sign * *pn / power;
+
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
+
+int main(void)
+{
+	double a;
+
+	while (getfloat(&a) != EOF)
+		printf("%f\n", a);
+
+	return 0;
+}
