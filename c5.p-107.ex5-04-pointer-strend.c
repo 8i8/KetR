9d49df82d8c46d19687a70542b337de7b6d7312d
Iain Hill <iainardernhill@gmail.com>
2017-04-10 09:38:06 +0200

    strend improved

--- c5.p-107.ex5-04-pointer-strend.c
+++ c5.p-107.ex5-04-pointer-strend.c
@@ -4,6 +4,11 @@
  */
 #include <stdio.h>
 
+/*
+ * Although this version of strend works, it is not at all preferable as there
+ * is an increased risk that the programmer leave the pointers in the middle
+ * of the string by forgetting to return them to the start of the string.
+ */
 int strend(char *s, char *t)
 {
 	size_t i, j = 0;
@@ -16,6 +21,39 @@ int strend(char *s, char *t)
 	if(i > j) {
 		while (j > 0) {
 			if (*--s == *--t) {
+				j--, i--;
+			} else  {
+				*s = *s-i, *t = *t-j;
+				return 0;
+			}
+		}
+		*s = *s-i;
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * Here the problem of returning the pointer is solved by making copy's of those
+ * pointers and using them during the operation in the place of the original
+ * pointers received as arguments.
+ */
+int _strend(char *s, char *t)
+{
+	size_t i, j = 0;
+
+	char *s1, *t1;
+
+	s1 = s, t1 = t;
+
+	while (*s1)
+		i++, *s1++;
+	while (*t1)
+		j++, *t1++;
+
+	if(i > j) {
+		while (j > 0) {
+			if (*--s1 == *--t1) {
 				j--;
 			} else 
 				return 0;
@@ -25,12 +63,38 @@ int strend(char *s, char *t)
 	return 0;
 }
 
+/*
+ * Here the problem is resolved by performing arithmetic on the integer i and j
+ * rather than moving the pointers.
+ */
+int __strend(char *s, char *t)
+{
+	size_t i, j = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	while (*(t + j) != '\0')
+		j++;
+
+	if(i > j) {
+		while (j > 0) {
+			if (*(s + --i) == *(t + --j)) {
+				;
+			} else 
+				return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+
 int main(void)
 {
 	char s[12] = { 'H','e','l','l','o',' ','W','o','r','l','d','\0' };
 	char t[6] = { 'w','o','r','l','d','\0' };
 
-	if (strend(s, t))
+	if (_strend(s, t))
 		puts("Yes");
 	else
 		puts("No");
