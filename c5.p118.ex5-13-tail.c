820937928001b4cc2ac9da6aac964db406fbef55
Iain Hill <iainardernhill@gmail.com>
2017-05-16 23:39:50 +0200

    tail array reduced for memory efficiency

--- c5.p118.ex5-13-tail.c
+++ c5.p118.ex5-13-tail.c
@@ -15,10 +15,13 @@
 #define MAXLINES	5000
 #define ALLOCSIZE	5000000
 
+static void testwrite(char *string, int start, int len);
+static void trimtail(char *lineptr[], int len, int tail);
 static int readlines(char *lineptr[], const int maxlines);
 static int getline(char *, int);
 static char *alloc(const int n);
-static void writetail(char *lineptr[], const int nlines, const int tailLen);
+static void afree(char *p);
+static void writelines(char *lineptr[], const int nlines);
 
 static char *lineptr[MAXLINES];			/* Pointer to text lines */
 static char allocbuf[ALLOCSIZE];		/* storage for alloc */
@@ -29,20 +32,48 @@ static char *allocp = allocbuf;			/* next free position */
  */
 int main(int argc, char *argv[])
 {
-	int nlines, tailLen;	/* number of input lines to read */
-	tailLen = 10;
+	int nlines, tail;	/* number of input lines to read */
+	tail= 10;
 
 	while (--argc > 1 && (*++argv)[0] == '-')
 		if ((*argv)[1] == 'n')
-			tailLen = atof(*++argv);
+			tail= atof(*++argv);
 
-	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-		writetail(lineptr, nlines, tailLen);
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+	{
+		trimtail(lineptr, nlines, tail);
+		/* Test that the memory is reset */
+		testwrite("Hello World!", ++tail, 11);
+		writelines(lineptr, tail);
 		return 0;
 	} else {
-		printf("Error: input to big to sort\n");
+		printf("Error: input to large\n");
 		return 1;
 	}
+}
+
+/*
+ * Write test string to char* array.
+ */
+static void testwrite(char* string, int start, int len)
+{
+	char *p;
+
+	p = alloc(len);
+	lineptr[start] = p;
+
+	strcpy(lineptr[start], string);
+}
+
+/*
+ * Trim back to required tail length.
+ */
+static void trimtail(char *lineptr[], int len, int tail)
+{
+	afree(lineptr[tail]);
+
+	while (tail--)
+		lineptr[tail] = lineptr[--len];
 }
 
 /*
@@ -56,6 +87,7 @@ static int readlines(char *lineptr[], const int maxlines)
 	char *p, line[MAXLEN];
 
 	nlines = 0;
+
 	while ((len = getline(line, MAXLEN)) > 0)
 		if (nlines >= maxlines || (p = alloc(len)) == NULL)
 			return -1;
@@ -98,14 +130,22 @@ static char *alloc(const int n)	/* return pointer to  characters */
 }
 
 /*
+ * Set memory pointer position, free memory.
+ */
+static void afree(char *p)
+{
+	if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+		allocp = p;
+}
+
+/*
  * Write output lines.
  */
-static void writetail(char *lineptr[], const int nlines, const int tailLen)
+static void writelines(char *lineptr[], const int nlines)
 {
 	size_t i;
 
 	for (i = 0; i < nlines; i++)
-		if (i >= nlines - tailLen) 
-			printf("%s\n", *(lineptr+i));
+		printf("%s\n", *(lineptr+i));
 }
 
